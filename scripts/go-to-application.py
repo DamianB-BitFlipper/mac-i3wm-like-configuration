1f00cde2d7079115c14be5c62338c3a177bf3586
Damian Barabonkov <damian@ellamind.com>
2024-08-08 10:55:25 +0200

    Added a regex window-title option to go-to-application

--- scripts/go-to-application.py
+++ scripts/go-to-application.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 # NOTE: On reboot, you may get an error that:
 # `Error running command: cannot focus space due to an error with the scripting-addition.`
 #
@@ -9,47 +8,39 @@
 # `sudo yabai --load-sa` ## Run this command twice for some reason
 #
 # Follow https://github.com/koekeishiya/yabai/issues/1158 for more info.
-
 import sys
+import argparse
+import re
 from script_utils import run_bash_command
 
-
-def get_window_data_from_application_name(
-    yabai_windows: dict, application_name: str
-) -> dict | None:
+def get_window_data(yabai_windows: dict, application_name: str, title_regex: str = None) -> dict | None:
     for window in yabai_windows:
         if window["app"] == application_name:
-            return window
-
-    # The application was not found
+            if title_regex is None or re.search(title_regex, window["title"]):
+                return window
+    # The application or matching window was not found
     return None
 
-
 def main():
-    # Get the application names from the command line arguments
-    if len(sys.argv) < 2:
-        print(f"Usage: {sys.argv[0]} <app_name1> [app_name2] [app_name3] ...")
-        return
-
-    app_names = sys.argv[1:]
+    parser = argparse.ArgumentParser(description="Focus on specified application windows.")
+    parser.add_argument("app_names", nargs="+", help="Names of the applications to focus on")
+    parser.add_argument("--window-title", help="Regex pattern to match window titles")
+    args = parser.parse_args()
 
     yabai_windows, ret_code = run_bash_command("yabai -m query --windows", json_output=True)
-
     if ret_code != 0 or yabai_windows is None:
         return
 
-    for app_name in app_names:
-        window = get_window_data_from_application_name(yabai_windows, app_name)
-
+    for app_name in args.app_names:
+        window = get_window_data(yabai_windows, app_name, args.window_title)
         # If the window was found, focus to it and exit
         if window is not None:
             run_bash_command(f"yabai -m space --focus {window['space']}")
             run_bash_command(f"yabai -m window --focus {window['id']}")
-            print(f"Focused on {app_name}")
+            print(f"Focused on {app_name}" + (f" (title matched: {window['title']})" if args.window_title else ""))
             return
 
-    print("None of the specified applications were found.")
-
+    print("None of the specified applications" + (" with matching window titles" if args.window_title else "") + " were found.")
 
 if __name__ == "__main__":
     main()
